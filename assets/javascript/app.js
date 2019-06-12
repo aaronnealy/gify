ea8faf867d20f5c96c7986041720f9844c1c5622
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-06-12 09:57:13 -0500

    Added working buttons and ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,40 @@
 //Create an array of strings related to a topic of my interests.
 
-//on click button
 
-var topics = ["Pizza", "Tacos", "Sushi", "Pasta", "French Fries"];
-console.log(topics);
+var topicsArr = ["Pizza", "Tacos", "Sushi", "Pasta", "French Fries"];
+console.log(topicsArr);
 
-//A function for displaying topic buttons
-function renderButtons(){
-    $("#buttons-view").empty();
-    for (var i = 0; i< topics.length; i++) {
-      var topicBtn = $("<button>");
-      topicBtn.text(topics[i]);
-      topicBtn.attr("class", "topic-btn");
-      topicBtn.attr("data-topicname", topics[i]);
-      $("#buttons-view").append(topicBtn);
-       
-    }
-}
-$(document).on("click", ".topic-btn", function (e) {
-    console.log(this);
-    var topicName = $(this).attr("data-topicname");
-    console.log(topicName)
-    // alert(movieName)
-    // Ajax call here / make sure to use the correct API key
-    var apiKey = "W2rfdCIplPQR1xAHGGIkKCxv1XxMx2V";
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=W2rfdCIplPQR1xAHGGIkKCxv1XxMx2V2&tag=&rating=G"
+var apiKey = "W2rfdCIplPQR1xAHGGIkKCxv1XxMx2V"
 
+//A on click button for displaying topic buttons
+$("button").on("click", function() {
+    var topic = $(this).attr("data-topic");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      topic + "&api_key=W2rfdCIplPQR1xAHGGIkKCxv1XxMx2V2&limit=10";
+    // var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=W2rfdCIplPQR1xAHGGIkKCxv1XxMx2V2&tag=&rating=G"
     $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response){
-          console.log(response);
-      });
-      
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+    console.log(response)
+    console.log(response.data[0])
+    console.log(response.data[0].rating)
+
+      var results = response.data
+
+      for (let i = 0; i < results.length; i++) {
+          var topicDiv = $("<div>");
+          
+          
+      }
+
+    // Ajax call here / make sure to use the correct API key
+    // ;
+    
+
+   
     
 })
+})
     // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-    // and display it in the div with an id of movie-view
+    // and display it in the div with an id of movie-
